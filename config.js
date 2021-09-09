4de27d20f2fdd159f35e9dea4d3fe49d4dcf7cc3
Paulius Jacinkevicius <paulius.jacinkevicius@cdn77.com>
2021-09-09 23:57:23 +0300

    whoops

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 export default {
-    "ZOOM_API_KEY": "Jw2BqWsTQ4-SxM0RaSpXJQ",
-    "ZOOM_API_SECRET": "dK4cDWZpTM0tFqDcIC5w2GbyOCdZ8cnCIzL5",
-    "ZOOM_USER_ID": "Lzu062GuSf63oMz4FQAYUQ",
-    "DESTINATION": "/Users/paulius/Desktop",
-    "LOCATION": "/Users/paulius/Public/Cedevita/Zoomer/zoomer"
+    "ZOOM_API_KEY": "",
+    "ZOOM_API_SECRET": "",
+    "ZOOM_USER_ID": "",
+    "DESTINATION": "",
+    "LOCATION": ""
 }
